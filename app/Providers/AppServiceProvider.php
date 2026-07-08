2c1664714aca71dbaaf847047bafc98c21fcf993
PanduAshidiq <pandudwi120@gmail.com>
2026-07-08 12:40:00 +0700

    feat: configure Scramble for API documentation and implement alphabetical tag sorting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,10 @@ namespace App\Providers;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Illuminate\Routing\Route;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -29,6 +32,21 @@ class AppServiceProvider extends ServiceProvider
         // Allow access to Scramble API Docs based on environment
         Gate::define('viewApiDocs', function (?User $user) {
             return config('app.env') === 'local' || filter_var(config('app.enable_api_docs'), FILTER_VALIDATE_BOOLEAN);
+        });
+
+        // Konfigurasi Scramble
+        Scramble::routes(function (Route $route) {
+            $uri = $route->uri();
+            return Str::startsWith($uri, 'v1/') && !Str::is('v1/migrate', $uri);
+        });
+
+        Scramble::extendOpenApi(function (OpenApi $openApi) {
+            // Sort tags alphabetically
+            $tags = $openApi->tags;
+            usort($tags, function ($a, $b) {
+                return strcmp($a->name, $b->name);
+            });
+            $openApi->tags = $tags;
         });
     }
 }
